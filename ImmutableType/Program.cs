887a037c97adc86dc4b3187f46e95a7a32b7366b
Ivan MSI <arigatory@gmail.com>
2022-02-15 01:22:57 +0300

    add some structure

---
+++ ImmutableType/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Int32 v = 5;
+            Console.WriteLine("{0}, {1}, {2}", v, v, v);
+
+            Object o = v;
+            Console.WriteLine("{0}, {1}, {2}", o, o, o);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
